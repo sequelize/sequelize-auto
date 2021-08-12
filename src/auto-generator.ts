4fbb4ddaf24c3560cc7ba5143851ad985e078465
Ed Carl <edwardmcarl@gmail.com>
2021-08-12 19:32:31 -0400

    generalize EXTRACT detection

--- src/auto-generator.ts
+++ src/auto-generator.ts
@@ -332,7 +332,7 @@ export class AutoGenerator {
 
           } else if (this.isNumber(field_type) || field_type.match(/^(json)/)) {
               // preserve as literal if this field is a function to extract a value from a timestamp
-              if (defaultVal.toLowerCase().indexOf('extract') === 0 || defaultVal.toLowerCase().indexOf('(extract') === 0) {
+              if (defaultVal.toLowerCase().includes('extract')) {
                 val_text = "Sequelize.Sequelize.literal('" + defaultVal + "')";
               } else {
                   // remove () around mssql numeric values; don't quote numbers or json
