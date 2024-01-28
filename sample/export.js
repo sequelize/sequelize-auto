cdd02d3882648d128ad9b4a979fe30a183dddc66
Honesty Nyong <crazibeatdeveloper@gmail.com>
2024-01-28 15:09:16 +0100

    #

--- sample/export.js
+++ sample/export.js
@@ -3,20 +3,11 @@ const SequelizeAuto = require('../index');
 // Edit your database settings in config.js
 const config = require('./config');
 
-const auto = new SequelizeAuto('afrikmart', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  directory: './models', // where to write files
-  port: '3306',
-  lang: 'ts',
-  caseModel: '0',
-  caseFile: '0',
-  noAlias: true,
-});
+var auto = new SequelizeAuto(config.dbname, config.user, config.pass, config.autoOptions);
 
 auto.run().then((data) => {
-  // const tableNames = Object.keys(data.tables);
-  // console.log(tableNames); // table list
+  const tableNames = Object.keys(data.tables);
+  console.log(tableNames); // table list
   // console.log(data.foreignKeys); // foreign key list
   // console.log(data.text)         // text of generated files
 });
